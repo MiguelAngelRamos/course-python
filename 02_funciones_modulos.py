775df647e92531d49db79926c382e7acda59e622
Miguel Ramos <return.infox@gmail.com>
2026-01-28 20:20:36 -0300

    funciones con parametros por default

--- 02_funciones_modulos.py
+++ 02_funciones_modulos.py
@@ -33,3 +33,26 @@ def sumar(a, b):
 print("\nLlamando a la función sumar:")
 resultado = sumar(5, 7)
 print(f"La suma de 5 y 7 es: {resultado}")
+
+# Valor Asoluto
+def valor_absoluto(numero):
+    if numero >=0:
+        return numero
+    else:
+        return -numero
+
+# Invocar la función valor_absoluto
+print("\nLlamando a la función valor_absoluto:")
+print(f"El valor absoluto de -10 es: {valor_absoluto(-10)}")
+print(f"El valor absoluto de 10 es: {valor_absoluto(10)}")
+
+# Parametros por defecto
+def crear_perfil(nombre, edad=18, pais="Chile"):
+    print(f"Nombre: {nombre}, Edad: {edad}, País: {pais}")
+
+# Invocar la función crear_perfil con diferentes combinaciones de argumentos
+print("\nLlamando a la función crear_perfil:")
+crear_perfil("Bob")
+crear_perfil("Sofia")
+crear_perfil("Charlie", 25)
+crear_perfil("Diana", 30, "Argentina")
