852463a5a238f3438813ce1923d18b940fd6dd9f
Miguel Ramos <return.infox@gmail.com>
2026-01-29 21:07:38 -0300

    base de codigo para actividad

--- actividad-guiada/actividad_guiada.py
+++ actividad-guiada/actividad_guiada.py
@@ -38,3 +38,38 @@ DESCUESTO_MAYORISTA = 0.15 # 15% de descuento para compras mayores a 10kg
 UMBRAL_MAYORISTA = 10.0 # Cantidad minina para ser considerado mayorista
 DESCUENTO_PRIMERA_COMPRA = 0.10 # 10% para nuevos clientes
 IMPUESTO = 0.19 # Impuesto sobre las ventas (19%)
+
+def mostrar_catalogo(inventario):
+    pass
+
+def validar_fruta_existe(nombre_fruta, inventario):
+    pass
+
+## S.O.L.I.D - S: Single Responsibility Principle
+def validar_cantidad(cantidad):
+    pass
+
+def calcular_subtotal(precio_unitario, cantidad):
+    pass
+
+def aplicar_descuentos(subtotal, cantidad, es_primera_compra):
+    pass
+
+def calcular_impuesto(precio_con_descuentos):
+    pass
+
+def actualizar_stock(nombre_fruta, cantidad, inventario):
+    pass
+
+def mostrar_recibo(nombre_fruta, cantidad, precio_unitario, subtotal, 
+                   descuento_porcentaje, monto_descuento, nombre_descuento, 
+                   precio_con_descuentos, impuesto, total_final):
+    pass
+
+def procesar_venta():
+    pass
+
+
+if __name__ == "__main__":
+    procesar_venta()
+    
