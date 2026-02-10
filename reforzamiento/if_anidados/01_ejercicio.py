0d6dd9d7f1445167c3dd71feb667b0b9265e35b7
Miguel Ramos <return.infox@gmail.com>
2026-02-10 20:05:29 -0300

    if anidados

---
+++ reforzamiento/if_anidados/01_ejercicio.py
@@ -0,0 +1,31 @@
+"""
+Ejercicio 1: Sistema de Descuentos en Tienda
+Enunciado: Una tienda departamental tiene un sistema de descuentos basado en el monto de compra y el tipo de membresía del cliente.
+
+Reglas:
+
+Si el cliente tiene membresía Premium:
+
+- Compras mayores a $1000: 25% de descuento
+- Compras entre $500 y $1000: 20% de descuento
+- Compras menores a $500: 15% de descuento
+
+Si el cliente tiene membresía Regular:
+
+- Compras mayores a $1000: 15% de descuento
+- Compras entre $500 y $1000: 10% de descuento
+- Compras menores a $500: 5% de descuento
+
+Si el cliente No tiene membresía:
+
+- Compras mayores a $1000: 5% de descuento
+- Compras menores a $1000: Sin descuento
+
+Tareas:
+
+- Solicitar al usuario el monto de la compra
+- Solicitar el tipo de membresía (Premium, Regular, Ninguna)
+- Calcular el descuento correspondiente
+- Mostrar el monto final a pagar
+
+"""
