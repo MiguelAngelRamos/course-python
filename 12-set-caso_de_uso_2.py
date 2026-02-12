37f930d300238f52e340f81cbbe6720139b6a220
Miguel Ramos <return.infox@gmail.com>
2026-02-11 21:12:15 -0300

    ejercicio terminado

--- 12-set-caso_de_uso_2.py
+++ 12-set-caso_de_uso_2.py
@@ -9,6 +9,18 @@ servidores = [
 
 def filtrar_servidores_activos(lista_servidores:list[dict]) -> list[dict]:
     return [servidor for servidor in lista_servidores if servidor["estado"] == "activo"]
+
+def calcular_vram_total(lista_servidores:list[dict]) -> int:
+    total = 0
+    for servidor in lista_servidores:
+        total = total + servidor["vram"] # total += servidor["vram"]
+    return total
+    # return sum(servidor["vram"] for servidor in lista_servidores)
+
+def mostrar_reporte(titulo:str, datos:list[dict], total_vram:int):
+    print(f"\n === {titulo.upper()} ===")
+    print(json.dumps(datos, indent=4)) # Imprime los datos en formato JSON con indentación
+    print(f"VRAM Total Disponible: {total_vram} GB")
 """
 lista_filtrada = []
     for servidor in lista_servidores:
@@ -17,5 +29,6 @@ lista_filtrada = []
     return lista_filtrada
 """
 if __name__ == "__main__":
-    servidores_operativos = filtrar_servidores_activos(servidores)
-    print("Servidores activos:", servidores_operativos)
+    servidores_activos = filtrar_servidores_activos(servidores)
+    vram_disponible = calcular_vram_total(servidores_activos)
+    mostrar_reporte("Reporte de Servidores Activos", servidores_activos, vram_disponible)
